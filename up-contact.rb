2799d4019cc34300f90b6ffe225d53b71495fef1
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-21 19:48:45 -0500

    ejercicio contactos

---
+++ up-contact.rb
@@ -0,0 +1,33 @@
+file_name = "contacts.txt"
+puts "Escribamos una nueva linea en el archivo #{file_name}"
+
+txt_file = open(file_name, 'a+')
+
+puts "Nombre del contacto"
+print '> '
+nombre = gets.chomp
+
+puts "Correo del contacto"
+print '> '
+correo = gets.chomp
+
+puts "Telefono del contacto"
+print '> '
+telefono = gets.chomp
+
+puts "Direccion del contacto"
+print '> '
+direccion = gets.chomp
+
+puts "Genero del contacto"
+print '> '
+genero = gets.chomp
+
+
+contacto = '"' + nombre + '"' + ", " + '"' + correo + '"' + ", " + '"' + telefono + '"' + ", " + '"' + direccion + '"' + ", " + '"' + genero + '"' + "\n"
+
+txt_file.write(contacto)
+
+txt_file.close
+
+puts "El archivo de contactos ha sido modificado"
